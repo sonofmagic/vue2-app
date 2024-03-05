1ad0587845687550fa05bc36bfe8c47c04dcf283
Yang QiMing <yangqiming@gigacloudtech.com>
2024-03-05 13:50:14 +0800

    xxxx

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { onMounted, ref, nextTick } from 'vue'
+import { onMounted, ref, nextTick, watch } from 'vue'
 import Swiper from 'swiper';
+import { nanoid } from 'nanoid'
 // import Swiper styles
 import 'swiper/css';
 
@@ -34,15 +35,41 @@ const items = ref([])
 function getRandomData() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      let res = page.value < 2 ? [1, 2, 3, 4, 5] : Array.from({ length: Math.floor(Math.random() * 5) }).fill(0).map((_, idx) => idx)
+      let res: string[]
+      switch (true) {
+        case page.value < 2: {
+          res = Array.from({ length: 5 }).fill(0).map((_, idx) => nanoid())
+          break
+        }
+        case page.value === 2: {
+          res = Array.from({ length: Math.floor(Math.random() * 5) }).fill(0).map((_, idx) => nanoid())
+          // isLoaded.value = true
+          break
+        }
+        default: {
+          res = []
+          break
+        }
+      }
+
       resolve(res)
     }, 800)
   })
 }
 
+// watch(isLoaded, () => {
+//   if (isLoaded.value) {
+//     nextTick(() => {
+//       swiper.value.update()
+//     })
+//   }
+// })
 
 onMounted(async () => {
   const res = await getRandomData()
+  if (res.length < 5) {
+    isLoaded.value = true
+  }
   items.value = res
 
   if (swiperDom.value) {
@@ -76,9 +103,18 @@ onMounted(async () => {
 // const swiper = new Swiper();
 
 
-function next() {
+async function next() {
   swiper.value.slideNext()
   console.log(swiper.value.realIndex, swiper.value.activeIndex)
+  page.value++
+  const res = await getRandomData()
+  if (res.length < 5) {
+    isLoaded.value = true
+  }
+  items.value.push(...res)
+  nextTick(() => {
+    swiper.value.update()
+  })
 }
 
 function prev() {
@@ -90,16 +126,18 @@ function prev() {
   <div class="container mx-auto pt-6">
     <div ref="swiperDom" class="swiper">
       <div class="swiper-wrapper">
-        <div class="swiper-slide" :key="idx" v-for="(item, idx) in items">
+        <div class="swiper-slide" :key="item" v-for="(item, idx) in items">
           <div
             class="w-[300px] h-[300px] border border-gray-400 rounded-lg flex items-center justify-center bg-sky-100">
             {{ idx }}</div>
         </div>
-        <div class="swiper-slide" :key="`g${idx}`" v-for="(idx) in 5">
-          <div class="w-[300px] h-[300px] border border-gray-400 rounded-lg flex items-center justify-center bg-red-50">
-            骨架屏{{ idx }}</div>
-        </div>
-
+        <template v-if="!isLoaded">
+          <div class="swiper-slide" :key="`g${idx}`" v-for="(idx) in 5">
+            <div
+              class="w-[300px] h-[300px] border border-gray-400 rounded-lg flex items-center justify-center bg-red-50">
+              骨架屏{{ idx }}</div>
+          </div>
+        </template>
       </div>
       <div class="absolute left-0 border border-yellow-400 rounded-full top-1/2 z-10 cursor-pointer" @click="prev">Left
       </div>
