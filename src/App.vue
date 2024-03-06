953da57131ccc31b9dfbb3079c899eebc7a4656c
ice breaker <1324318532@qq.com>
2024-03-06 23:22:33 +0800

    dd

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import Swiper from './components/Swiper.vue';
+import CalendarRange from './components/CalendarRange.vue';
 </script>
 
 <template>
-  <div class="container mx-auto pt-6">
-    <Swiper></Swiper>
+  <div class="container mx-auto pt-6 space-y-4">
+    <div>
+      <Swiper></Swiper>
+    </div>
+
+    <div>
+      <CalendarRange></CalendarRange>
+    </div>
+
   </div>
 </template>
 
